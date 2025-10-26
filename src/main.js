b25e73f5ebeee9e8911983ae5106193fc7387d0c
greasebob <thecrazygamer425@gmail.com>
2025-10-26 17:30:03 -0400

    added html code

--- src/main.js
+++ src/main.js
@@ -1,8 +1,3 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-
 document.querySelector('#app').innerHTML = `
   <div>
     <a href="https://vite.dev" target="_blank">
